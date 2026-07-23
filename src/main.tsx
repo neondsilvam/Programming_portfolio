c6174a11ad3381770939448676cd3529f199cc12
Julian D Silva <neon.d.silvam@gmail.com>
2026-07-22 18:00:01 -0700

    FIX: Deploy resolved

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import {HashRouter} from 'react-router-dom'
 import App from './App'
-import {HashRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <HashRouter>
